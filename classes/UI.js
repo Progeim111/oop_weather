54b7ce456b360f25e8231559923d4043c99a872e
Progeim111 <102576858+Progeim111@users.noreply.github.com>
2022-11-10 09:37:10 +0200

    Added css and more data

--- classes/UI.js
+++ classes/UI.js
@@ -3,6 +3,12 @@ class UI {
         this.description = document.querySelector("#description")
         this.temp = document.querySelector("#temp")
         this.city = document.querySelector("#location")
+        this.wind_deg = document.querySelector("#wind_deg")
+        this.wind_sp = document.querySelector("#wind_speed")
+        this.humi = document.querySelector('#humidity')
+        this.air_pr = document.querySelector('#air_pressure')
+        this.visibility = document.querySelector('#visibility')
+
     }
 
     drawWeather(data) {
@@ -10,6 +16,11 @@ class UI {
 
         this.description.innerHTML = data.weather[0].description
         this.temp.innerHTML = celsius + "&deg;"
-        this.city.innerHTML = data.name
+        this.city.innerHTML = 'City: ' + data.name
+        this.wind_deg.innerHTML = 'Wind ' + data.wind.deg + '\u00B0'
+        this.wind_sp.innerHTML = 'Wind speed: ' + data.wind.speed + ' m/s'
+        this.humi.innerHTML = 'Humidity: ' + data.main.humidity + ' %'
+        this.air_pr.innerHTML = 'Air pressure: ' + data.main.pressure
+        this.visibility.innerHTML = 'Visibility: ' + data.visibility
     }
 }
